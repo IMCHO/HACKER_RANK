fa91f8788ebbacf3a290a65b7bf68c95763c3a33
IMCHO <dlsxor21c@naver.com>
2018-08-06 23:40:36 +0900

    Practicing python

---
+++ Python_Practice/practice2.py
@@ -0,0 +1,9 @@
+num=int(input())
+if num%2==1:
+    print('Weird')
+else:
+    if num>=2 and num<=5:
+        print('Not Weird')
+    elif num>=6 and num<=20:
+        print('Weird')
+    else: print('Not Weird')
